7a0eb3ae99bdcfcea3c7856cbb5d01c434b02b88
WilliamKoza <william.koza@gmail.com>
2018-05-05 11:06:02 +0200

    init

--- src/app/lazy/lazy.component.ts
+++ src/app/lazy/lazy.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {LazyService} from './lazy.service';
-import {AnotherService} from './another.service';
 // import {AnotherService} from './another.service';
 
 
